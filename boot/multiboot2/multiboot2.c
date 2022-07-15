a31224c071eb8192f1ba6d2768005c749bc7319f
vizdl <vizdl@qq.com>
2022-07-15 23:48:55 +0800

    格式化 multiboot2.c

--- boot/multiboot2/multiboot2.c
+++ boot/multiboot2/multiboot2.c
@@ -83,20 +83,20 @@ void cmain(unsigned long magic, unsigned long addr)
 			break;
 		case MULTIBOOT_TAG_TYPE_MODULE:
 			cprintf("Module at 0x%x-0x%x. Command line %s\n",
-				   ((struct multiboot_tag_module *)tag)->mod_start,
-				   ((struct multiboot_tag_module *)tag)->mod_end,
-				   ((struct multiboot_tag_module *)tag)->cmdline);
+					((struct multiboot_tag_module *)tag)->mod_start,
+					((struct multiboot_tag_module *)tag)->mod_end,
+					((struct multiboot_tag_module *)tag)->cmdline);
 			break;
 		case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
 			cprintf("mem_lower = %uKB, mem_upper = %uKB\n",
-				   ((struct multiboot_tag_basic_meminfo *)tag)->mem_lower,
-				   ((struct multiboot_tag_basic_meminfo *)tag)->mem_upper);
+					((struct multiboot_tag_basic_meminfo *)tag)->mem_lower,
+					((struct multiboot_tag_basic_meminfo *)tag)->mem_upper);
 			break;
 		case MULTIBOOT_TAG_TYPE_BOOTDEV:
 			cprintf("Boot device 0x%x,%u,%u\n",
-				   ((struct multiboot_tag_bootdev *)tag)->biosdev,
-				   ((struct multiboot_tag_bootdev *)tag)->slice,
-				   ((struct multiboot_tag_bootdev *)tag)->part);
+					((struct multiboot_tag_bootdev *)tag)->biosdev,
+					((struct multiboot_tag_bootdev *)tag)->slice,
+					((struct multiboot_tag_bootdev *)tag)->part);
 			break;
 		case MULTIBOOT_TAG_TYPE_MMAP:
 		{
@@ -108,12 +108,12 @@ void cmain(unsigned long magic, unsigned long addr)
 				 (multiboot_uint8_t *)mmap < (multiboot_uint8_t *)tag + tag->size;
 				 mmap = (multiboot_memory_map_t *)((unsigned long)mmap + ((struct multiboot_tag_mmap *)tag)->entry_size))
 				cprintf(" base_addr = 0x%x%x,"
-					   " length = 0x%x%x, type = 0x%x\n",
-					   (unsigned)(mmap->addr >> 32),
-					   (unsigned)(mmap->addr & 0xffffffff),
-					   (unsigned)(mmap->len >> 32),
-					   (unsigned)(mmap->len & 0xffffffff),
-					   (unsigned)mmap->type);
+						" length = 0x%x%x, type = 0x%x\n",
+						(unsigned)(mmap->addr >> 32),
+						(unsigned)(mmap->addr & 0xffffffff),
+						(unsigned)(mmap->len >> 32),
+						(unsigned)(mmap->len & 0xffffffff),
+						(unsigned)mmap->type);
 		}
 		break;
 		case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
